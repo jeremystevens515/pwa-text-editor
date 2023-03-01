b7348e5348d6708c5c4e6a0586226409eec923f2
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-03-01 16:59:12 -0500

    commit prior to deployment

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,48 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB("jate", 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains("jate")) {
+				console.log("jate database already exists");
+				return;
+			}
+			db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+			console.log("jate database created");
+		},
+	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+	console.log("Post to the database");
+
+	// create connection to the database and version we want to use
+	const jateDb = await openDB("jate", 1);
+
+	// create new transaction and specify the database and data privileges
+	const tx = jateDb.transaction("jate", "readwrite");
+
+	// open the desired objectstore
+	const store = tx.objectStore("jate");
+
+	// use .add() method on the store and pass in the content
+	const request = store.put({ id: 1, value: content });
+
+	// get confirmation
+	const result = await request;
+	console.log("Data saved to the database");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+	console.log("GET all data from the database");
+	const jateDb = await openDB("jate", 1);
+	const tx = jateDb.transaction("jate", "readonly");
+	const store = tx.objectStore("jate");
+	const request = store.getAll();
+	const result = await request;
+	console.log("result.value", result);
+	return result.value;
+};
 
 initdb();
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -23,8 +23,24 @@ module.exports = () => {
 				title: "Webpack Plugin",
 			}),
 			new InjectManifest({
-				swSrc: "./scr/src-sw.js",
-				swDest: "service-worker.js",
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js",
+			}),
+			new WebpackPwaManifest({
+				name: "Just Another Text Editor",
+				short_name: "JATE",
+				description: "Edit text",
+				background_color: "#7eb4e2",
+				theme_color: "#7eb4e2",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
 			}),
 		],
 
@@ -32,7 +48,7 @@ module.exports = () => {
 			rules: [
 				{
 					test: /\.css$/i,
-					user: ["style-loader", "css-loader"],
+					use: ["style-loader", "css-loader"],
 				},
 				{
 					test: /\.(png|svg|jpg|jpeg|gif)$/i,
